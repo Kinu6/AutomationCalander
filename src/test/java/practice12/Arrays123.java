1f2d172beb0747b0f671f6e181165a515c57e87d
Kinu6 <kravish.keshri7@yahoo.com>
2025-04-22 14:33:16 +0530

    arrays

---
+++ src/test/java/practice12/Arrays123.java
@@ -0,0 +1,10 @@
+package practice12;
+
+public class Arrays123 {
+
+	public static void main(String[] args) {
+		Object ar[]; //This declares an array reference memory null
+		ar=new Object []{1,"ravish"}; // memory given here
+	}
+
+}
